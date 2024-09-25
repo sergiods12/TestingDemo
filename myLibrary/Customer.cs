445d7e45563f4c5734aa064e60deb8f816bfdbf1
Sergio Diez <sergiods12@educastur.es>
2024-09-25 11:02:56 +0200

    Finished Customer class

--- myLibrary/Customer.cs
+++ myLibrary/Customer.cs
@@ -3,7 +3,9 @@
     public class Customer
     {
 
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
 
-
+        public string FullName() => $"{FirstName} {LastName}";
     }
 }
